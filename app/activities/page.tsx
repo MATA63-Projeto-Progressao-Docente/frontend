93100a780c4ee7412edca2cc075e9a55747f47a4
Linsmar <linsmarvital@gmail.com>
2023-12-05 21:48:35 -0300

    fix: mocka em um state

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -17,30 +17,7 @@ export default function ActivitiesPage() {
 	const [hours, setHours] = useState('');
 	const [description, setDescription] = useState('');
 	const [file, setFile] = useState();
-
-	const handleFileChange = (event: any) => {
-    const newFile = event.target.files[0];
-    setFile(newFile);
-  };
-
-  // const router = useRouter();
-	// const token = localStorage.getItem('authToken');
-	// let isAuthenticated = false;
-	// if (token !== 'undefined') {
-	// 	isAuthenticated = true;
-	// }
-
-	// useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     router.push('/login');
-  //   }
-  // }, [isAuthenticated, router]);
-
-	// const activities = api.get('/activities').then((res) => {
-	// 	console.log(res)
-	// })
-
-	const activities = [
+	const [activities, setActivities] = useState([
 		{
 			name: "Lorem ipsum",
 			number: 1,
@@ -101,7 +78,44 @@ export default function ActivitiesPage() {
 			points: 20,
 			fieldId: 10
 		},
-	]
+	]);
+
+	const handleFileChange = (event: any) => {
+    const newFile = event.target.files[0];
+    setFile(newFile);
+  };
+
+	const handleSubmit = (event: any) => {
+    event.preventDefault();
+		setActivities(prevActivities => [
+			...prevActivities,
+			{
+				name: title,
+				number: 777,
+				points: 20,
+				fieldId: 777
+			}
+		]);
+		console.log(activities)
+		setIsModalOpen(false);
+  };
+
+  // const router = useRouter();
+	// const token = localStorage.getItem('authToken');
+	// let isAuthenticated = false;
+	// if (token !== 'undefined') {
+	// 	isAuthenticated = true;
+	// }
+
+	// useEffect(() => {
+  //   if (!isAuthenticated) {
+  //     router.push('/login');
+  //   }
+  // }, [isAuthenticated, router]);
+
+	// const activities = api.get('/activities').then((res) => {
+	// 	console.log(res)
+	// })
 
 	const NavBar: React.FC = () => {
 		return (
@@ -123,7 +137,7 @@ export default function ActivitiesPage() {
 				<div className='flex items-center gap-4 flex-wrap px-40 py-20'>
 					{
 						activities.map((activity, index) => (
-							<div key={index} className='flex flex-col border-2 rounded border-secondary px-6 py-3'>
+							<div key={index} className='flex flex-col border-2 rounded border-secondary px-6 py-3 w-2/12'>
 								<h2>{activity.name}</h2>
 								<p>{activity.fieldId}</p>
 								<p className='self-end'>{activity.points} pontos</p>
@@ -142,7 +156,7 @@ export default function ActivitiesPage() {
 				>
 					<p className='absolute right-0 mr-5 text-primary text-2xl cursor-pointer' onClick={() => setIsModalOpen(false)}>X</p>
 					<h2 className='mt-20'>Nova atividade</h2>
-					<form className='flex flex-col mt-20'>
+					<form onSubmit={handleSubmit} className='flex flex-col mt-20'>
 						<div className='flex gap-x-4 mb-10'>
 							<label className='w-6/12'>
 								<select value={field} onChange={(event) => setField(event.target.value)} className='border-2 rounded border-secondary w-full'>
